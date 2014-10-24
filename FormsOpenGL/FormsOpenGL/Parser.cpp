8c1d07e78fc2c358e1eaa291633aa703c313582d
Vladimir Maryenkov <maryenkov4eg@gmail.com>
2014-10-24 18:27:18 +0300

    add parse via ITEM (TODO parse method)

--- FormsOpenGL/FormsOpenGL/Parser.cpp
+++ FormsOpenGL/FormsOpenGL/Parser.cpp
@@ -21,29 +21,45 @@ const std::shared_ptr<std::list<std::shared_ptr<View>>> Parser::parse(const char
 	if (file.is_open()) {
 		View* v = nullptr;
 		while (getline(file, currentLine)) {
+			int start = -1;
+			int end = -1;
 			int current = 0;
+
+
+			size_t rectPosition = currentLine.find("<Rectangle>");
+			if (currentLine.find("<Rectangle>"))
 			for (std::string::iterator it = currentLine.begin(); it != currentLine.end(); ++it, ++current) {
-				if (*it == ' ')
+				
+
+				
+				/*
+				//DEVIDER_MARK
+				if ((*it) == DEVIDER_MARK) {
 					continue;
-
-				if ((*it) == OPEN_START_TAG) {
-					++it;
-					int start = ++current;
-					for (; it != currentLine.end(); ++it, ++current)
-						if ((*it) == CLOSE_START_TAG) {
-						parseRectangle(file, currentLine, current);
-							/*std::shared_ptr<std::string> nameValue = std::shared_ptr<std::string>(new std::string(currentLine.substr(start, current - start)));
-							std::shared_ptr<std::string> name = std::shared_ptr<std::string>(new std::string("name"));
-							std::shared_ptr<std::pair<std::string, std::string>> pair = std::shared_ptr<std::pair<std::string, std::string>>(new std::pair<>);*/
-					}
 				}
-
+				//OPEN_START_TAG
+				if ((*it) == OPEN_START_TAG) {
+					start = current+1;
+					continue;
+				}
+				//CLOSE_START_TAG
+				if ((*it) == CLOSE_START_TAG) {
+					end = current;
+					std::string view = currentLine.substr(start, current - start);
+					std::cout << "VIEW is " << view << std::endl;
+					if ("Rectangle" == view)
+						parseRectangle(file, currentLine, current);
+				}
+				*/
+				/*std::shared_ptr<std::string> nameValue = std::shared_ptr<std::string>(new std::string(currentLine.substr(start, current - start)));
+				std::shared_ptr<std::string> name = std::shared_ptr<std::string>(new std::string("name"));
+				std::shared_ptr<std::pair<std::string, std::string>> pair = std::shared_ptr<std::pair<std::string, std::string>>(new std::pair<>);*/
 			}
 
 
 
 
-			size_t start = currentLine.find_first_of(OPEN_START_TAG)+1;
+			/*size_t start = currentLine.find_first_of(OPEN_START_TAG)+1;
 			size_t end = currentLine.find_first_of(CLOSE_START_TAG);
 			if (start != std::string::npos && end != std::string::npos && start < end) {
 				std::string currentItem = currentLine.substr(start, end - start);
@@ -87,8 +103,7 @@ const std::shared_ptr<std::list<std::shared_ptr<View>>> Parser::parse(const char
 					}
 				}
 				viewList->push_back(std::shared_ptr<View>(v));
-			}
-			std::cout << currentLine << std::endl;
+			}*/
 		}
 	}
 
@@ -103,6 +118,7 @@ void Parser::parseRectangle(std::ifstream& file, std::string currentLine, int cu
 			std::string var;
 			std::string value;
 			int start = -1;
+			int end = -1;
 			for (std::string::iterator it = currentLine.begin() + current; it != currentLine.end(); ++it, ++current) {
 				//DEVIDER_MARK
 				if ((*it) == DEVIDER_MARK) {
@@ -111,25 +127,37 @@ void Parser::parseRectangle(std::ifstream& file, std::string currentLine, int cu
 				//ASSING_MARK
 				if ((*it) == ASSING_MARK) {
 					if (start != -1) {
-						var = currentLine.substr(start, current - start);
+						if (end == -1)		//if only one symbol
+							end = start;
+						// +1 because we have not increase "end" on latest iteration
+						var = currentLine.substr(start, end - start + 1); 
 						std::cout << "var = " << var << std::endl;
+						start = -1;
+						end = -1;
 					}
-					start = -1;
 					continue;
 				}
 				//VALUE_MARK
 				if ((*it) == VALUE_MARK) {
 					if (start != -1) {
-						value = currentLine.substr(start, current - start);
+						if (end == -1)		//if only one symbol
+							end = start;
+						// +1 because we have not increase "end" on latest iteration
+						value = currentLine.substr(start, end - start + 1);
 						std::cout << "value = " << value << std::endl;
 						start = -1;
+						end = -1;
 					}
 					continue;
 				}
+				if ((*it) == OPEN_END_TAG[0] && (*(it+1)) == OPEN_END_TAG[1]) {
+
+				}
 				if (start == -1)
 					start = current;
+				else
+					end = current;
 			}
 			current = 0;
-			start = 0;
 		} while (getline(file, currentLine));
 }
